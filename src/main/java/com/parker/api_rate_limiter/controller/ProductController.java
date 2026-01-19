1d1c3f5e0ebc0fa78c15f4595487c7da2698fd35
Parker Downing <parkeraydowning@gmail.com>
2026-01-19 15:30:33 -0700

    Adds REST controller that returns mock data

---
+++ src/main/java/com/parker/api_rate_limiter/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.parker.api_rate_limiter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.ArrayList;
+
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+  List<String> users = new ArrayList<>();
+
+  @GetMapping("/products")
+  public List<String> getProducts()
+  {
+    return List.of("Product 1", "Product 2", "Product 3");
+  }
+
+  @GetMapping("/users")
+  public List<String> getUsers()
+  {
+    return List.of("user1", "user2", "user3");
+  }
+
+  @PostMapping("/users")
+  public String createUser(String user)
+  {
+    users.add(user);
+    return user;
+  }
+}
