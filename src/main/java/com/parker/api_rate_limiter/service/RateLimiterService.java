e60f74a5dd894f03769b97bac905c3917e77bc2e
Parker Downing <parkeraydowning@gmail.com>
2026-01-24 11:12:52 -0700

    Adds allowRequest() -

--- src/main/java/com/parker/api_rate_limiter/service/RateLimiterService.java
+++ src/main/java/com/parker/api_rate_limiter/service/RateLimiterService.java
@@ -1,6 +1,7 @@
 package com.parker.api_rate_limiter.service;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.time.Instant;
 
 import org.springframework.stereotype.Service;
 
@@ -18,4 +19,37 @@ public class RateLimiterService {
     maxRequests = 10;
     windowMs = 60000; // 1 minute
   }
+
+  public boolean allowRequest(String identifier)
+  {
+    
+    RateLimitEntry entry = rateLimits.get(identifier);
+    long now = Instant.now().getEpochSecond();
+
+    // If it doesn't exist, create a new RateLimitEntry. Use current timestamp and count of 1
+    if (entry == null || now > entry.getWindowStart() + windowMs)
+    {
+      entry = new RateLimitEntry(1, now);
+      rateLimits.put(identifier, entry);
+      return true;
+    }
+
+    if (entry.getCount() >= maxRequests)
+    {
+      return false;
+    }
+
+    entry.setCount(entry.getCount() + 1);
+    return true;
+  }
+
+  // public int getRemainingRequests(String identifier)
+  // {
+
+  // }
+
+  // public long getResetTime(String identifier)
+  // {
+
+  // }
 }
