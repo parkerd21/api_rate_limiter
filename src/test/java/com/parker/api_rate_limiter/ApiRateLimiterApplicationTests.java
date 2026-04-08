5fecbdf6736d3f124911aa89409727e546884e4f
Parker Downing <parkeraydowning@gmail.com>
2026-04-07 21:43:49 -0600

    Updates tests to use memory profile

--- src/test/java/com/parker/api_rate_limiter/ApiRateLimiterApplicationTests.java
+++ src/test/java/com/parker/api_rate_limiter/ApiRateLimiterApplicationTests.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("memory")
 class ApiRateLimiterApplicationTests 
 {
 
